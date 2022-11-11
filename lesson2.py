4079a0c53229792a60a4a388454d9c875852dee1
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-11-11 17:09:41 +0200

    lesson 2

---
+++ lesson2.py
@@ -0,0 +1,43 @@
+# 1)написати функцію на замикання котра буде в собі зберігати список справ, вам потрібно реалізувати два методи:
+# - перший записує в список нову справу
+# - другий повертає всі записи
+def notebook() -> list:
+    todo_list: list = []
+
+    def add_todo(todo: str) -> list:
+        todo_list.append(todo)
+        return todo_list
+
+    def all_todo() -> list:
+        nonlocal todo_list
+        return todo_list
+
+    return [add_todo, all_todo]
+
+
+add, all = notebook()
+
+
+# 3) створити функцію котра буде повертати сумму розрядів числа у вигляді строки (також використовуемо типізацію)
+#
+# Приклад:
+#
+# expanded_form(12) # return '10 + 2'
+# expanded_form(42) # return '40 + 2'
+# expanded_form(70304) # return '70000 + 300 + 4'
+
+def expanded_form(a: int) -> list:
+    b = list(str(a))
+    res = [c for c in b]
+    i = 0
+    l = len(res)
+    while i < l+i:
+        res[i] = res[i] + '0' * (l - 1)
+        l = l - 1
+        i = i + 1
+        print(l)
+    res = '+'.join(res)
+    return res
+
+
+print(expanded_form(21342332535))
